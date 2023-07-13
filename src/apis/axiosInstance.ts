11ce8fb7137eb96deb4a2035766c232934ddbdd0
Telescope <77131309+DoDevet@users.noreply.github.com>
2023-07-13 23:33:29 +0900

    Update axiosInstance.ts

--- src/apis/axiosInstance.ts
+++ src/apis/axiosInstance.ts
@@ -8,7 +8,7 @@ const client = axios.create({
   headers: {
     Accept: "application/vnd.github+json",
     "X-Github-Api-Version": "2022-11-28",
-    // Authorization: `Bearer ${process.env.REACT_APP_TOKEN}`,
+    Authorization: `Bearer ${process.env.REACT_APP_TOKEN}`,
   },
 });
 
