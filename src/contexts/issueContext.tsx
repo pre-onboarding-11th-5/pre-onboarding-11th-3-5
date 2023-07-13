e8bca3213371f162ec9652ee66dd486e0df11ffe
aggie97 <4ggie1997@gmail.com>
2023-07-13 23:41:27 +0900

    Refactor: issueContext state seperation
    Separate the object state into individual states.

--- src/contexts/issueContext.tsx
+++ src/contexts/issueContext.tsx
@@ -29,53 +29,47 @@ export const useIssueListDispatch = () => {
 };
 
 const IssueProvider: React.FC<React.PropsWithChildren> = ({ children }) => {
-  const [data, setData] = useState<IssueState>({
-    data: [],
-    loading: true,
-    error: null,
-    currentPage: 0,
+  const [data, setData] = useState<IssueList>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<{ message: string } | null>(null);
+  const [pageState, setPageState] = useState<{
+    currentPage: number;
+    hasNextPage: boolean;
+  }>({
+    currentPage: 1,
     hasNextPage: true,
   });
 
   const dispatchIssueList = async (page: number) => {
     try {
-      setData((prev) => ({ ...prev, loading: true, error: null }));
+      setLoading(true);
+      setError(null);
 
       const { data } = await getIssueList(page);
 
       setData((prev) => {
-        if (prev.currentPage === page) return prev;
-        return {
-          ...prev,
-          data: [...prev.data, ...data],
-          currentPage: page,
-          hasNextPage: data.length === 30,
-        };
+        if (pageState.currentPage === page) return prev;
+        return [...prev, ...data];
       });
+      setPageState({ currentPage: page, hasNextPage: data.length === 30 });
     } catch (e) {
       if (isAxiosError<ErrorResponse>(e) && e.response) {
         const {
           data: { message },
         } = e.response;
-        setData((prev) => ({
-          ...prev,
-          error: { message },
-        }));
+        setError({ message });
       } else if (isAxiosError(e)) {
         const { message } = e;
-        setData((prev) => ({
-          ...prev,
-          error: { message },
-        }));
+        setError({ message });
       }
     } finally {
-      setData((prev) => ({ ...prev, loading: false }));
+      setLoading(false);
     }
   };
 
   const fetchNextPage = () => {
-    if (data.loading || !data.hasNextPage) return;
-    dispatchIssueList(data.currentPage + 1);
+    if (loading || !pageState.hasNextPage) return;
+    dispatchIssueList(pageState.currentPage + 1);
   };
 
   useEffect(() => {
@@ -83,7 +77,9 @@ const IssueProvider: React.FC<React.PropsWithChildren> = ({ children }) => {
   }, []);
 
   return (
-    <IssueListStateContext.Provider value={data}>
+    <IssueListStateContext.Provider
+      value={{ data, loading, error, ...pageState }}
+    >
       <IssueListDispatchContext.Provider value={fetchNextPage}>
         {children}
       </IssueListDispatchContext.Provider>
