1e8764b05bfadf332d4a7b05e6c2eadbf93b9307
Kim Do Hyeong <me@m4nd4r1n.dev>
2023-07-13 18:52:59 +0900

    feat: Error 컴포넌트

---
+++ src/components/common/Error/Error.styles.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const ErrorBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 8px;
+  padding: 16px 0;
+`;
+
+export const ErrorText = styled.span`
+  font-size: large;
+  color: red;
+`;
