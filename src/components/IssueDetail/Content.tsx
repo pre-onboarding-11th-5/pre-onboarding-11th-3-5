ac7eb41c961f95fa286141fbd9dc5e51d0c592a2
Kim Do Hyeong <me@m4nd4r1n.dev>
2023-07-13 22:01:13 +0900

    feat: Content 컴포넌트

--- src/components/IssueDetail/Content.tsx
+++ src/components/IssueDetail/Content.tsx
@@ -1,5 +1,23 @@
+import { ReactMarkdown } from "react-markdown/lib/react-markdown";
+import { useIssueDetail } from "../../contexts/issueDetailContext";
+import { ContentBox } from "./Content.styles";
+import remarkGfm from "remark-gfm";
+import "github-markdown-css/github-markdown-light.css";
+import rehypeRaw from "rehype-raw";
+
 function Content() {
-  return <div>Content</div>;
+  const { data } = useIssueDetail();
+  return data ? (
+    <ContentBox>
+      <ReactMarkdown
+        className="markdown-body"
+        remarkPlugins={[remarkGfm]}
+        rehypePlugins={[rehypeRaw]}
+      >
+        {data.body}
+      </ReactMarkdown>
+    </ContentBox>
+  ) : null;
 }
 
 export default Content;
