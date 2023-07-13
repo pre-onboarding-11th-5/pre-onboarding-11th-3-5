7de76192347911a12f895a34113282a1a7fb1e48
Kim Do Hyeong <me@m4nd4r1n.dev>
2023-07-13 17:26:29 +0900

    feat: IssueDetail 컴포넌트

--- src/components/IssueDetail/index.tsx
+++ src/components/IssueDetail/index.tsx
@@ -1,12 +1,33 @@
 import Content from "./Content";
 import IssueItem from "../common/IssueItem";
+import {
+  useIssueDetail,
+  useIssueDetailDispatch,
+} from "../../contexts/issueDetailContext";
+import Loading from "../common/Loading";
+import Error from "../common/Error";
+import { IssueDetailBox } from "./IssueDetail.styles";
 
 function IssueDetail() {
+  const { data, error, loading } = useIssueDetail();
+  const fetchIssueDetail = useIssueDetailDispatch();
+
   return (
-    <div>
-      <IssueItem isAvatar />
+    <IssueDetailBox>
+      {data && (
+        <IssueItem
+          comments={data.comments}
+          created_at={data.created_at}
+          login={data.user.login}
+          number={data.number}
+          title={data.title}
+          avatar_url={data.user.avatar_url}
+        />
+      )}
       <Content />
-    </div>
+      <Loading loading={loading} />
+      <Error error={error} refetch={fetchIssueDetail} />
+    </IssueDetailBox>
   );
 }
 
