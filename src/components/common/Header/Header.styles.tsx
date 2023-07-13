e85a69d57df6ef5b802314f53f70b54745f9b992
Kim Do Hyeong <me@m4nd4r1n.dev>
2023-07-13 22:00:58 +0900

    feat: Header 컴포넌트

---
+++ src/components/common/Header/Header.styles.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const HeaderBox = styled.header`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 999;
+  padding: 16px 0;
+  background-color: #fff;
+  text-align: center;
+`;
